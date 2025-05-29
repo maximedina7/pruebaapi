4fc14865c8ea379998eb0861de4a795430ae471a
maximedina7 <maxidomedina@gmail.com>
2025-05-29 16:32:08 -0300

    .

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -6,7 +6,13 @@ export default function CoffeeCard({ item, isFavorite, onToggleFavorite }) {
       <img src={item.image} alt={item.title} />
       <h2>{item.title}</h2>
       <p><strong>Descripción:</strong> {item.description || 'Sin descripción.'}</p>
-      <p><strong>Ingredientes:</strong> {item.ingredients?.join(', ') || 'No especificados.'}</p>
+      <p>
+  <strong>Ingredientes:</strong>{' '}
+  {Array.isArray(item.ingredients) && item.ingredients.length > 0
+    ? item.ingredients.join(', ')
+    : 'No especificados.'}
+</p>
+
       <button onClick={onToggleFavorite}>
         {isFavorite ? '💖 Favorito' : '🤍 Agregar a Favoritos'}
       </button>
